122192d04ed88493459a6f84b2eababce74ff6d7
abitwhy <736490428@qq.com>
2022-03-23 10:54:05 +0800

    perf: update

--- src/components/AComponent.vue
+++ src/components/AComponent.vue
@@ -55,21 +55,3 @@ export default defineComponent({
   inheritAttrs: false,
 })
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
