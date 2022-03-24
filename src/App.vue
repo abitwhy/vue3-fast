4a014f27ba3ca6e991a55f4f7513974d7fbb9b2d
abitwhy <736490428@qq.com>
2022-03-24 09:07:05 +0800

    chore: auto import
    global API and Componet

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import AComponent from './components/AComponent.vue'
-</script>
-
 <template>
   <AComponent task="Validate props" worker="Vue with TypeScript" />
 </template>
