688553ce583c761d80874b7b789bb76bdcca3b1f
abitwhy <736490428@qq.com>
2022-03-25 11:07:35 +0800

    chore: path alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import Pages from 'vite-plugin-pages'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
