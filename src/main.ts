f0c31ba0cd7e90ec6454f1b82045fdaef7b79d98
abitwhy <736490428@qq.com>
2022-03-25 16:10:43 +0800

    refactor: move store install detail

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import stores from './stores'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(stores).mount('#app')
