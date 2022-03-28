7e85c09451cf0efee3ea19f11dca9c6c3aa44863
abitwhy <736490428@qq.com>
2022-03-28 17:01:51 +0800

    refactor: global types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,12 @@
-/// <reference types="vite/client" />
+/** vite 定义全局常量替换方式
+ * 见：https://cn.vitejs.dev/config/#define
+ * 替代方式（推荐）：在 `tsconfig.json` 配置 `compilerOptions.types`
+ * 见：https://www.tslang.cn/docs/handbook/tsconfig-json.html#:~:text=%40types%EF%BC%8CtypeRoots%E5%92%8Ctypes
+ */
+/* /// <reference types="vite/client" /> */
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
