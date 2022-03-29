1b55156048899b02ce6b5fc4f336ae19375cd78a
abitwhy <736490428@qq.com>
2022-03-29 10:51:57 +0800

    docs: update tsconfig#types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /** vite 定义全局常量替换方式
  * 见：https://cn.vitejs.dev/config/#define
  * 替代方式（推荐）：在 `tsconfig.json` 配置 `compilerOptions.types`
- * 见：https://www.tslang.cn/docs/handbook/tsconfig-json.html#:~:text=%40types%EF%BC%8CtypeRoots%E5%92%8Ctypes
+ * 见：https://www.typescriptlang.org/tsconfig#types
  */
 /* /// <reference types="vite/client" /> */
 
