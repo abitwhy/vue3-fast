42da06038651d225acf36381351fc05624a28c5c
abitwhy <736490428@qq.com>
2022-04-06 08:43:19 +0800

    feat(learn): ref

---
+++ src/pages/learn/ref.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>count:{{ count }}</p>
+  <p>obj:{{ obj }}</p>
+  <button @click="count++">count++</button>
+  <button @click="obj.count++">obj.count++</button>
+</template>
+
+<script setup lang="ts">
+const count = ref(0)
+const obj = reactive({ count })
+</script>
