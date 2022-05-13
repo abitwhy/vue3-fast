6df9e41bb30c333a3445c03f097be2ca42195fce
abitwhy <736490428@qq.com>
2022-05-13 10:50:44 +0800

    docs: update comment

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,17 @@
-/** vite 定义全局常量替换方式
- * 见：https://cn.vitejs.dev/config/#define
- * 替代方式（推荐）：在 `tsconfig.json` 配置 `compilerOptions.types`
- * 见：https://www.typescriptlang.org/tsconfig#types
+/** vite 默认环境全局类型声明文件
+ *
+ * @note 引入方式
+ * - （推荐）在 `tsconfig.json` 配置 `compilerOptions.types`
+ * @doc https://www.typescriptlang.org/tsconfig#types
+ * - 在本文件中使用三斜线指令引入
+ * @doc https://ts.xcatliu.com/basics/declaration-files.html#%E4%B8%89%E6%96%9C%E7%BA%BF%E6%8C%87%E4%BB%A4
+ * @doc https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html
  */
 /* /// <reference types="vite/client" /> */
 
+/** 定义所有文件类型为 vue（即 vue 单文件组件）的类型
+ * 由 vite cli 模板预设生成
+ */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
