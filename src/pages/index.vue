b25860d7a874ef412ce36fa54f96df4dc1143f3e
abitwhy <736490428@qq.com>
2022-05-16 16:17:21 +0800

    refactor: translate to English
    use English in whole project except comment(for the moment)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,1 +1,6 @@
-<template>体验开发 Vue3+Vite+Typescript 项目的快感</template>
+<template>
+  A project based on:
+  <a href="https://vuejs.org/">Vue3</a>,
+  <a href="https://vitejs.dev/">Vite</a> and
+  <a href="https://www.typescriptlang.org/">Typescript</a>.
+</template>
