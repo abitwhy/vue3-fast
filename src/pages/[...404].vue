51f64fee93a21229b3b6d75efa3df77d2cd29180
abitwhy <736490428@qq.com>
2022-05-27 10:05:03 +0800

    refactor: replace $router

--- src/pages/[...404].vue
+++ src/pages/[...404].vue
@@ -2,11 +2,13 @@
   <h1>Sorry, page does not existed.</h1>
   <code>{{ url }}</code> is an invalid URL. Please check your input.
   <p>
-    <button @click="$router.go(-1)">Go Back</button>
-    <button @click="$router.push('/')">Go Home</button>
+    <button @click="router.go(-1)">Go Back</button>
+    <button @click="router.push('/')">Go Home</button>
   </p>
 </template>
 
 <script setup lang="ts">
+import router from '~/router'
+
 const url = window.location.href
 </script>
