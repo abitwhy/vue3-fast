24f4995222b98934bde8e16a503d06c3b27cb130
abitwhy <abitwhy@qq.com>
2022-06-05 03:37:57 +0800

    perf: update

--- src/pages/learn/import-json.vue
+++ src/pages/learn/import-json.vue
@@ -2,7 +2,8 @@
   <h1>The way to import a json file</h1>
   <ul>
     <li>Ues `fetch()`</li>
-    <li>Ues import (a ES6 proposal)</li>
+    <li>Ues import with assert(a ES6 proposal)</li>
+    <li>Ues import (TS)</li>
   </ul>
 </template>
 
@@ -15,11 +16,19 @@ onBeforeMount(async () => {
   tsconfig.value = await res.json()
 })
 
-/** import json file
+/** import json file ES
  *
  * @note 这是一个 ES6 提案
  *
  * @doc https://es6.ruanyifeng.com/?search=%E6%8E%A5%E5%8F%A3&x=0&y=0#docs/proposals#JSON-%E6%A8%A1%E5%9D%97
  */
 // import tsconfig from '/tsconfig.json' assert { type: "json" };
+
+/** import json file TS
+ * tsconfig.json 里开启 resolveJsonModule 配置即可使用
+ *
+ * @doc https://www.typescriptlang.org/tsconfig#resolveJsonModule
+ */
+// import tsconfig from '/tsconfig.json'
+// console.log(JSON.stringify(tsconfig))
 </script>
