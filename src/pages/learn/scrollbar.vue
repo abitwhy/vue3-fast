7c857aec5e9076479a5ef5bdc5a67ee29b54ed3b
abitwhy <736490428@qq.com>
2022-06-29 15:45:00 +0800

    feat(page): scrollbar

---
+++ src/pages/learn/scrollbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="content"></div>
+  </div>
+</template>
+<style lang="scss">
+::-webkit-scrollbar {
+  width: 6px;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 99px;
+  background: #cccccc;
+}
+.container {
+  overflow-y: auto;
+  height: 40vh;
+  .content {
+    height: 200%;
+    background: radial-gradient(#f4f7c8, #a09fc7);
+  }
+}
+</style>
